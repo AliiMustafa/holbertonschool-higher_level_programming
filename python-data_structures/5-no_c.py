7b408fbca91cee2f55588ebc2d187c57647ef012
AliiMustafa <aliimustafazada@gmail.com>
2024-09-11 15:51:10 +0400

    Remove c in string

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -2,7 +2,7 @@
 def no_c(my_string):
     a = list(my_string)
     for i in a:
-        if i == "c" or i == "C":
+        if (i == "c") or (i == "C"):
             a.remove(i)
     a = "".join(a)
     my_string = a
