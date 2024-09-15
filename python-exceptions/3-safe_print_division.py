bb2d3f1286082f68b14eae9f4a1a5977e536c308
AliiMustafa <aliimustafazada@gmail.com>
2024-09-15 19:45:26 +0400

    division

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
-from typing import final
-
-
 def safe_print_division(a, b):
-    result  = None
+    result = None
     try:
         result = a / b
     except ZeroDivisionError:
-        return None
+        return result
     finally:
         print("Inside result: {}".format(result))
         return result
