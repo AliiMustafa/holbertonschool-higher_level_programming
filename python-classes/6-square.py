bd95f40d8f403eb06fd3079066b5d28e9e92dc76
AliiMustafa <aliimustafazada.com>
2024-09-19 18:48:08 +0400

    Add position

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -7,13 +7,6 @@ class Square:
     def __init__(self, size=0, position=(0, 0)):
         self.__size = size
         self.__position = position
-        for x in range(len(position)):
-            if not isinstance(position[x], int) or x == 2 or position[x] < 0 or not isinstance(position, tuple):
-                raise TypeError("position must be a tuple of 2 positive integers")
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
 
     def area(self):
         return self.__size ** 2
