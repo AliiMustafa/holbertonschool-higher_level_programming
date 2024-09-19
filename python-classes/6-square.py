e2c2e347ca27c5d172caab29298a5abdfefde701
AliiMustafa <aliimustafazada.com>
2024-09-19 19:23:42 +0400

    Add position

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -15,6 +15,10 @@ class Square:
     def size(self):
         return self.__size
 
+    @property
+    def position(self):
+        return self.__position
+
     @size.setter
     def size(self, value):
         if not isinstance(value, int):
@@ -22,10 +26,6 @@ class Square:
         elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
-
-    @property
-    def position(self):
-        return self.__position
 
     @position.setter
     def position(self, value):
