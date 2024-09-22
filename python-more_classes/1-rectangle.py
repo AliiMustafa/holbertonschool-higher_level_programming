442db47964a3e9b7cc24b3ec7680c5cf9e9094fc
AliiMustafa <aliimustafazada.com>
2024-09-22 20:18:24 +0400

    Adding width and height

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Module for creating rectangle class"""
+
+
+class Rectangle:
+    """Creating rectangle class"""
+
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
