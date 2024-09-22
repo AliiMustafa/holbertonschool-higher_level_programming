9370c23e2464b191a0a4498284880ab1cac04885
AliiMustafa <aliimustafazada.com>
2024-09-22 20:28:34 +0400

    Adding width and height

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -21,24 +21,14 @@ class Rectangle:
     def width(self, value):
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("width must be >= 0")
-        else:
-            self.__width = value
+        self.__width = value
 
     @height.setter
     def height(self, value):
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("height must be >= 0")
-        else:
-            self.__height = value
-try:
-    my_rectangle = Rectangle(2, -3)
-except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
-try:
-    my_rectangle = Rectangle(-2, 3)
-except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
+        self.__height = value
