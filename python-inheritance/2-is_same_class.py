e8c9e15de956e0792b8b9d9984a6f99e7aa07211
AliiMustafa <aliimustafazada.com>
2024-09-25 09:55:16 +0400

    Instance

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module for check instance"""
+
+
+def is_same_class(obj, a_class):
+    """Function for check instance"""
+    if isinstance(obj, a_class):
+        return True
+    return False
