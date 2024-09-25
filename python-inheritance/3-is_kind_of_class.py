379650186f05b8f9d27293dfc7b109884f51f8b5
AliiMustafa <aliimustafazada.com>
2024-09-25 09:59:57 +0400

    Instance

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module for instance"""
+
+
+def is_kind_of_class(obj, a_class):
+    """Function ofr instance"""
+    if isinstance(obj, a_class):
+        return True
+    return False
