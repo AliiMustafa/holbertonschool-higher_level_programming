2d1beaafefc37dd7273bae3a78f86a0b1cf94587
AliiMustafa <aliimustafazada.com>
2024-09-25 13:02:53 +0400

    Advanced tasks

--- python-inheritance/101-add_attribute.py
+++ python-inheritance/101-add_attribute.py
@@ -4,7 +4,6 @@
 
 def add_attribute(obj, name: str, val: str):
     """Function to add attribute to an object"""
-
     if not hasattr(obj, "__dict__"):
         raise TypeError("can't add new attribute")
     return setattr(obj, name, val)
