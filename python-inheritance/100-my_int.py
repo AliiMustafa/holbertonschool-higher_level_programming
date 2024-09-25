3ed62ea1acc99cad420556ce2cff8cc186b32f3b
AliiMustafa <aliimustafazada.com>
2024-09-25 13:02:27 +0400

    Advanced tasks

--- python-inheritance/100-my_int.py
+++ python-inheritance/100-my_int.py
@@ -6,9 +6,7 @@ class MyInt(int):
     """MyInt class"""
 
     def __eq__(self, __value: object):
-        """Making reverse =="""
         return super().__ne__(__value)
 
     def __ne__(self, __value: object):
-        """Making reverse !="""
         return super().__eq__(__value)
