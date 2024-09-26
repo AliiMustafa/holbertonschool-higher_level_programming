f9c6c5354d2491b3f31730910556cd40b7ddbf5d
AliiMustafa <aliimustafazada.com>
2024-09-26 17:25:20 +0400

    creating shape abc class

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -43,4 +43,5 @@ class Rectangle(Shape):
 def shape_info(obj):
     area = obj.area()
     perimeter = obj.perimeter()
-    print(f"Area: {area}\nPerimeter: {perimeter}")
+    print(f"Area: {area})")
+    print(f"Perimeter: {perimeter}")
