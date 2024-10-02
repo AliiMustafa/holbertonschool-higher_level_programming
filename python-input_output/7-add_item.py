f9ac81947ff8bc6d122c8a23ef4baf313b805b2a
AliiMustafa <aliimustafazada@gmail.com>
2024-10-02 16:19:02 +0400

    Add all arguments

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Module for add all arguments"""
+from sys import argv
+
+
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+file = "add_item.json"
+
+my_list = []
+my_list.extend(argv[1:])
+save_to_json_file(my_list, file)
