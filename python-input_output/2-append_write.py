8366ddc928ac40ca12064fb9b2dc30077c66fd7a
AliiMustafa <aliimustafazada@gmail.com>
2024-10-02 15:22:48 +0400

    Append write

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module for write data"""
+
+
+def append_write(filename="", text=""):
+    """Function for append write data"""
+    with open(filename, encoding="UTF-8") as f:
+        data = f.write(text)
+        return data
