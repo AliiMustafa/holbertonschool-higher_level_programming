466a9c74da8d049079c846e5de08cc6d622e97bc
AliiMustafa <aliimustafazada@gmail.com>
2024-10-02 17:11:27 +0400

    Add dict in json

---
+++ python-input_output/11-student.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Module for class to json"""
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__
+        new = {}
+        for at in attrs:
+            if at in self.__dict__:
+                new[at] = self.__dict__[at]
+        return new
+
+    def reload_from_json(self, json):
+        for key in self.__dict__.keys():
+            for value in json.keys():
+                if key == value:
+                    self.__dict__[key] = json[value]
