1aa27e7c723a09c3337854d37869b4715cfe6a1a
AliiMustafa <aliimustafazada@gmail.com>
2024-10-02 15:18:51 +0400

    Write file

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""Module for write data in file"""
+
+
 def write_file(filename="", text=""):
+    """function for write data in text"""
     with open(filename, "x", encoding="utf-8") as f:
         data = f.write(text)
         return data
