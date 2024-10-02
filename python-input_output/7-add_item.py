9bc8e4b6cfd97969fc9c3a62ed9961ea803319ae
AliiMustafa <aliimustafazada@gmail.com>
2024-10-02 16:29:23 +0400

    Add all arguments

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -7,6 +7,6 @@ save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 file = "add_item.json"
 
-my_list = []
+my_list = load_from_json_file(file)
 my_list.extend(argv[1:])
 save_to_json_file(my_list, file)
