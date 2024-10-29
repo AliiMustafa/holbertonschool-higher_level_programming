5d04bdad148348974659ddfd60723e85d171f882
AliiMustafa <aliimustafazada.com>
2024-10-29 19:59:05 +0400

    Without join

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,1 +1,4 @@
-SELECT cities.id, cities.name, states.name FROM cities INNER JOIN states ON cities.state_id = states.id ORDER BY cities.id;
+-- SQl
+SELECT cities.id, cities.name, states.name
+FROM cities JOIN states ON cities.state_id = states.id
+ORDER BY cities.id;
