70b1ff15e86ddc95307dcf71e823e1f01e667dda
AliiMustafa <aliimustafazada.com>
2024-11-03 21:08:32 +0400

    All files

--- python-object_relational_mapping/relationship_city.py
+++ python-object_relational_mapping/relationship_city.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
-"""Module containing City class"""
-from sqlalchemy import Column, Integer, String, ForeignKey
+"""Module containing State class"""
+from sqlalchemy import Column, String, Integer
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
 
-class City(Base):
-    """The City class"""
-    __tablename__ = 'cities'
+class State(Base):
+    """The State class"""
+    __tablename__ = 'states'
 
     id = Column(Integer, primary_key=True, nullable=False,
                 autoincrement=True, unique=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
